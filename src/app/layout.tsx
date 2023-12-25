391351a490871ce0c396e0a2613bfa2a7ae1a07e
Muhibbullah Ansary <muhibbullah611@gmail.com>
2023-12-26 03:07:44 +0600

    folder structure change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
 import './globals.css'
-import Header from '@/app/layouts/fragments/header/Header'
+import Header from '@/app/fragments/header/Header'
 
 const poppins = Poppins({
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
