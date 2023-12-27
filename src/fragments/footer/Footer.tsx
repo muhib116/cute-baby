a9ab2e104c29fc4570a7f3a7c0f721be0b3e81cf
MahsinUllahAzmi <azmiullahmahsin@gmail.com>
2023-12-27 20:08:55 +0600

    footer icon design

--- src/fragments/footer/Footer.tsx
+++ src/fragments/footer/Footer.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { FacebookLogo, GoogleChromeLogo, InstagramLogo, TwitterLogo, YoutubeLogo } from '@phosphor-icons/react'
+import { EnvelopeSimple, FacebookLogo, GoogleChromeLogo, InstagramLogo, MapPin, Phone, TwitterLogo, YoutubeLogo } from '@phosphor-icons/react'
 import React from 'react'
 
 function Footer() {
@@ -9,8 +9,8 @@ function Footer() {
       <div className='bg-[#181818]'>
         <div className='py-[60px] grid grid-cols-5 container mx-auto'>
           <div className='px-4'>
-            <h5 className=' text-base text-white mb-5 font-medium'>Information</h5>
-            <div className='font-light text-sm text-[#a7a7a7]'>
+            <h5 className='text-white mb-5 font-medium'>Information</h5>
+            <div className='font-light text-[0.8rem] text-[#a7a7a7]'>
               <a href="#" className='block mb-2'>About Us</a>
               <a href="#" className='block mb-2'>Delivery Information</a>
               <a href="#" className='block mb-2'>Privacy Policy</a>
@@ -19,8 +19,8 @@ function Footer() {
             </div>
           </div>
           <div className='px-4'>
-            <h5 className=' text-base text-white mb-5 font-medium'>Extras</h5>
-            <div className='font-light text-sm text-[#a7a7a7]'>
+            <h5 className='text-white mb-5 font-medium'>Extras</h5>
+            <div className='font-light text-[0.8rem] text-[#a7a7a7]'>
               <a href="#" className='block mb-2'>Brands</a>
               <a href="#" className='block mb-2'>Gift Certificates</a>
               <a href="#" className='block mb-2'>Affiliate</a>
@@ -29,8 +29,8 @@ function Footer() {
             </div>
           </div>
           <div className='px-4'>
-            <h5 className=' text-base text-white mb-5 font-medium'>My Account</h5>
-            <div className='font-light text-sm text-[#a7a7a7]'>
+            <h5 className='text-white mb-5 font-medium'>My Account</h5>
+            <div className='font-light text-[0.8rem] text-[#a7a7a7]'>
               <a href="#" className='block mb-2'>My Account</a>
               <a href="#" className='block mb-2'>Order History</a>
               <a href="#" className='block mb-2'>Wish List</a>
@@ -39,8 +39,8 @@ function Footer() {
             </div>
           </div>
           <div className='px-4'>
-            <h5 className=' text-base text-white mb-5 font-medium'>Quick Links</h5>
-            <div className='font-light text-sm text-[#a7a7a7]'>
+            <h5 className='text-white mb-5 font-medium'>Quick Links</h5>
+            <div className='font-light text-[0.8rem] text-[#a7a7a7]'>
               <a href="#" className='block mb-2'>Seller Login</a>
               <a href="#" className='block mb-2'>Seller Sign Up</a>
               <a href="#" className='block mb-2'>Seller Handbook</a>
@@ -49,12 +49,20 @@ function Footer() {
             </div>
           </div>
           <div className='px-4'>
-            <h5 className=' text-base text-white mb-5 font-medium'>Contact</h5>
-            <div className='font-light text-sm text-[#a7a7a7]'>
-              <a href="#" className='block mb-2'>Portable - Mega Store United States</a>
-              <a href="#" className='block mb-2'>000-000-0000</a>
-              <a href="#" className='block mb-2'>123456</a>
-              <a href="#" className='block mb-2'>sales@yourcompany.com</a>
+            <h5 className='text-white mb-5 font-medium'>Contact</h5>
+            <div className='font-light text-[0.8rem] text-[#a7a7a7]'>
+              <a href='#' className='mb-2 items-center flex gap-1'>
+                <MapPin size={21} className='w-5'/>
+                Mymensingh
+              </a>
+              <a href='#' className='mb-2 items-center flex gap-1'>
+                <Phone size={21} className='w-5'/>
+                017 7098-9591
+              </a>
+              <a href='#' className='mb-2 items-center flex gap-1'>
+                <EnvelopeSimple size={21} className='w-5'/>
+                shop@gmail.com
+              </a>
             </div>
           </div>
         </div>
