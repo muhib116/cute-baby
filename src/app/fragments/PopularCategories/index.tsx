871c4058b6523c13edb61370b39e17486e62c097
Muhibbullah Ansary <muhibbullah611@gmail.com>
2024-01-02 06:53:29 +0600

    minor change

--- src/app/fragments/PopularCategories/index.tsx
+++ src/app/fragments/PopularCategories/index.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import HorizontalCardList from '@/fragments/horizontalCardList/HorizontalCardList'
-import { popularCategoriesData } from '@/placeholderData/data'
 import CategoryCard from './CategoryCard'
 import { fetchData } from '@/apiConfig'
-import Loader from './Loader'
 
 const getData = async () => {
   const { data } = await fetchData('categories/list')
@@ -12,9 +10,6 @@ const getData = async () => {
 async function PopularCategories() {
   const data = await getData()
   return (
-    !data.length ?
-    <Loader />
-    :
     <HorizontalCardList
       itemsWrapperClassName='grid-cols-2 sm:grid-cols-5 lg:grid-cols-7 xl:grid-cols-8'
       title="Popular Categories"
